a8bafebc8740cbf9ed0f6418c9194949fcc54f97
Emma Cadavra <emmacadavra@gmail.com>
2023-07-08 19:09:26 +0000

    Add COLOUR_CHOICES constant with all 6 colour options (actual colours to be added later)

--- run.py
+++ run.py
@@ -3,22 +3,35 @@
 Code Breaker in the style of Mastermind.
 A code of four colours will be randomly generated from six colour choices.
 Objective: Guess the correct code within the allowed number of attempts.
-Choices: Red, Green, Blue, Yellow, Purple, White 
+Choices: Red (R), Green (G), Blue (B), Yellow (Y), Purple (P), White (W) 
 """
 
 # imports
 import random
 
 # constants
+COLOUR_CHOICES = ["R", "G", "B", "Y", "P", "W"]
+
 
 # menu
 
+
+
 # function to generate code
+
+
 
 # function for user input
 
+
+
 # function to compare user input with code
+
+
 
 # display user input with result of comparison
 
+
+
 # main game code
+
