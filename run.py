0f03d0030b1329c8434f8ab615628e572914f6d3
Emma Cadavra <emmacadavra@gmail.com>
2023-07-08 18:48:45 +0000

    Add commented sections to lay out initial logical steps

--- run.py
+++ run.py
@@ -1,8 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Code Breaker in the style of Mastermind.
+A code of four colours will be randomly generated from six colour choices.
+Objective: Guess the correct code within the allowed number of attempts.
+Choices: Red, Green, Blue, Yellow, Purple, White 
+"""
 
-# Project: Code breaker game (Mastermind, but that's a trademark).
-# Objective: Guess combination of four colours to "break the code"
-
+# imports
 import random
+
+# constants
+
+# menu
+
+# function to generate code
+
+# function for user input
+
+# function to compare user input with code
+
+# display user input with result of comparison
+
+# main game code
