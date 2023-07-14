e7df14bdf2fc24e085e1ff31e0599a840679bc18
Emma Cadavra <emmacadavra@gmail.com>
2023-07-14 10:28:51 +0000

    Separate Colors into separate colors.py file

---
+++ classes/colors.py
@@ -0,0 +1,20 @@
+# colour class
+class Colors:
+    """
+    ANSI escape codes to be used with their respective colours
+    """
+    default = "\033[0;37;40m"
+    red = "\033[1;31;40m"
+    green = "\033[1;32;40m"
+    yellow = "\033[1;33;40m"
+    blue = "\033[1;34;40m"
+    pink = "\033[1;35;40m"
+    cyan = "\033[1;36;40m"
+    white = "\033[1;37;40m"
+    red_block = "\033[1;31;41m"
+    green_block = "\033[1;32;42m"
+    yellow_block = "\033[1;33;43m"
+    blue_block = "\033[1;34;44m"
+    pink_block = "\033[1;35;45m"
+    cyan_block = "\033[1;36;46m"
+    white_block = "\033[1;37;47m"
