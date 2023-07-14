d6122c9d3dc969c5de65ebf33a85c09a371d9279
Emma Cadavra <emmacadavra@gmail.com>
2023-07-14 10:01:50 +0000

    Move main logo to graphics.py

--- run.py
+++ run.py
@@ -10,38 +10,7 @@ COLOUR_CHOICES = ["R", "G", "Y", "B", "P", "W"]
 CODE_LENGTH = 4
 MAX_ATTEMPTS = 9
 
-# colour class
-class Colours:
-    """
-    ANSI escape codes to be used with their respective colours
-    """
-    default = "\033[0;37;40m"
-    red = "\033[1;31;40m"
-    green = "\033[1;32;40m"
-    yellow = "\033[1;33;40m"
-    blue = "\033[1;34;40m"
-    pink = "\033[1;35;40m"
-    cyan = "\033[1;36;40m"
-    white = "\033[1;37;40m"
-    red_block = "\033[1;31;41m"
-    green_block = "\033[1;32;42m"
-    yellow_block = "\033[1;33;43m"
-    blue_block = "\033[1;34;44m"
-    pink_block = "\033[1;35;45m"
-    cyan_block = "\033[1;36;46m"
-    white_block = "\033[1;37;47m"
 
-
-def main_logo():
-    print(f"""{Colours.yellow}
-    
-                      _             ___          _      
-      /\/\   __ _ ___| |_ ___ _ __ / __\___   __| | ___ 
-     /    \ / _` / __| __/ _ \ '__/ /  / _ \ / _` |/ _ \
-    / /\/\ \ (_| \__ \ ||  __/ | / /__| (_) | (_| |  __/
-    \/    \/\__,_|___/\__\___|_| \____/\___/ \__,_|\___|
-                                                    
-{Colours.default}\n""")
 # menu/title screen
 def main_menu():
     """
