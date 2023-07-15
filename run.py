db9e4f9a7c57b5675f05624b3f8cf665916ed357
Emma Cadavra <emmacadavra@gmail.com>
2023-07-15 16:23:50 +0000

    Undo changes to text (additional space before sentences)

--- run.py
+++ run.py
@@ -21,8 +21,8 @@ def main_menu():
     """
     clear_screen()
     graphics.main_logo()
-    print(" ~*~ Press '1' to Play   ~*~   Press '2' for Instructions ~*~")
-    print(" ~*~ Press '3' for the Triforce   ~*~   Press '4' to Exit ~*~")
+    print("~*~ Press '1' to Play   ~*~   Press '2' for Instructions ~*~")
+    print("~*~ Press '3' for the Triforce   ~*~   Press '4' to Exit ~*~")
 
     while True:
         try:
@@ -68,16 +68,16 @@ def instructions():
     graphics.header_image()
 
     # display the game instructions to the player
-    print(f" A secret code of 4 colors will be randomly generated from the following colors:\n")
+    print(f"A secret code of 4 colors will be randomly generated from the following colors:\n")
     print(f"    {Colors.red}Red  {Colors.green}Green  {Colors.blue}Blue  {Colors.yellow}Yellow  {Colors.purple}Purple  {Colors.white}White{Colors.default}\n")
-    print(" Your heroic challenge is to find the solution in 12 attempts or fewer.")
-    print(" Remember: a color may appear more than once in the secret code!")
-    print(" Only type guesses that are 4 characters long with no commas or spaces.\n")
-    print(" Make sure you only use the following characters for their respective colors:\n")
+    print("Your heroic challenge is to find the solution in 12 attempts or fewer.")
+    print("Remember: a color may appear more than once in the secret code!")
+    print("Only type guesses that are 4 characters long with no commas or spaces.\n")
+    print("Make sure you only use the following characters for their respective colors:\n")
     print(f"    {Colors.red}R  {Colors.green}G  {Colors.blue}B  {Colors.yellow}Y  {Colors.purple}P  {Colors.white}W{Colors.default}")
     print(f"    {Colors.red_block} {Colors.default}  {Colors.green_block} {Colors.default}  {Colors.blue_block} {Colors.default}  {Colors.yellow_block} {Colors.default}  {Colors.purple_block} {Colors.default}  {Colors.white_block} {Colors.default}\n")
-    print(f" {Colors.white}Go forth, Hero, and become the MasterCode Breaker!{Colors.default}")
-    print(" Are you ready? ('Y' to play, 'N' to return to the menu)")
+    print(f"{Colors.white}Go forth, Hero, and become the MasterCode Breaker!{Colors.default}")
+    print("Are you ready? ('Y' to play, 'N' to return to the menu)")
 
     # call the play_again function to direct player to game/menu depending on their choice
     play_again()
@@ -155,25 +155,25 @@ def run_game():
         position, color = check_result(player_guess, modified_answer)
         if position == CODE_LENGTH:
             graphics.you_win()
-            print(f" The secret code is: {display_player_guess(original_answer)}\n")
-            print(" You cracked the code like a true Hero, and became the MasterCode Breaker!")
-            print(f" You defeated this challenge in {Colors.white}{attempt_number + 1}{Colors.default} attempt(s).\n")
-            print(" Would you like to try to become the MasterCode Breaker once more? (Y/N)")
+            print(f"The secret code is: {display_player_guess(original_answer)}\n")
+            print("You cracked the code like a true Hero, and became the MasterCode Breaker!")
+            print(f"You defeated this challenge in {Colors.white}{attempt_number + 1}{Colors.default} attempt(s).\n")
+            print("Would you like to try to become the MasterCode Breaker once more? (Y/N)")
             play_again()
             break
 
         elif attempt_number == MAX_ATTEMPTS and position != CODE_LENGTH:
             graphics.game_over()
-            print(f" The secret code was: {display_player_guess(original_answer)}\n")
-            print(" You were defeated this time, but do not despair! Try again? (Y/N)")
+            print(f"The secret code was: {display_player_guess(original_answer)}\n")
+            print("You were defeated this time, but do not despair! Try again? (Y/N)")
             play_again()
             break
 
         else:
             attempt_number += 1
-            print(f" Correct color and position: {position}")
-            print(f" Correct color, but incorrect position: {color}")
-            print(f" You have {Colors.white}{MAX_ATTEMPTS - attempt_number + 1}{Colors.default} attempt(s) remaining.\n")
+            print(f"Correct color and position: {position}")
+            print(f"Correct color, but incorrect position: {color}")
+            print(f"You have {Colors.white}{MAX_ATTEMPTS - attempt_number + 1}{Colors.default} attempt(s) remaining.\n")
 
 
 # function for player input
@@ -184,21 +184,21 @@ def player_guess_input(attempt):
     """
     while True:
         try:
-            guess = input(f" Enter your prediction ( {Colors.red}R {Colors.green}G {Colors.blue}B {Colors.yellow}Y {Colors.purple}P {Colors.white}W{Colors.default} ):\n").upper()
+            guess = input(f"Enter your prediction ( {Colors.red}R {Colors.green}G {Colors.blue}B {Colors.yellow}Y {Colors.purple}P {Colors.white}W{Colors.default} ):\n").upper()
             print("")
             guess_list = [g for g in guess]
 
             if len(guess_list) != CODE_LENGTH:
-                raise ValueError(f" Invalid - please enter {CODE_LENGTH} valid characters.\n" +
-                                 f" Please choose only from the following characters, with no commas or spaces:\n" +
-                                 f" {Colors.red}R  {Colors.green}G  {Colors.blue}B  {Colors.yellow}Y  {Colors.purple}P  {Colors.white}W{Colors.default}\n")
+                raise ValueError(f"Invalid - please enter {CODE_LENGTH} valid characters.\n" +
+                                 f"Please choose only from the following characters, with no commas or spaces:\n" +
+                                 f"{Colors.red}R  {Colors.green}G  {Colors.blue}B  {Colors.yellow}Y  {Colors.purple}P  {Colors.white}W{Colors.default}\n")
                 continue
 
             for character in guess_list:
                 if character not in COLOR_CHOICES:
-                    raise ValueError(f" Error: You have entered one or more invalid characters.\n" +
-                                     f" Please choose only from the following characters, with no commas or spaces:\n" +
-                                     f" {Colors.red}R  {Colors.green}G  {Colors.blue}B  {Colors.yellow}Y  {Colors.purple}P  {Colors.white}W{Colors.default}\n")
+                    raise ValueError(f"Error: You have entered one or more invalid characters.\n" +
+                                     f"Please choose only from the following characters, with no commas or spaces:\n" +
+                                     f"{Colors.red}R  {Colors.green}G  {Colors.blue}B  {Colors.yellow}Y  {Colors.purple}P  {Colors.white}W{Colors.default}\n")
             break
 
         except ValueError as e:
